4cfff83079255f1c74f14e1faff3cac59b2c35db
Giuseppe Boi <giuseppe@scandit.com>
2025-05-02 12:06:40 +0200

    Sample implemented

--- BLEScannerSDKSample/ViewController.swift
+++ BLEScannerSDKSample/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import BLEScannerSDK
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var textView: UITextView!
+
+    private var host: WedgeHost?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        host = WedgeHost(
+            hostName: "My Host Name",
+            projectCode: "---INSERT-YOUR-PROJECT-CODE-HERE---",
+            baseUrl: "https://express.scandit.com/bluetooth",
+            eventCallback: { [weak self] event in
+                guard let self else {
+                    return
+                }
+
+                let text = "Event: \(event.eventType), \(String(describing: event.data))\n"
+                textView.text = textView.text + text
+                print(text)
+            }
+        )
+
+        imageView.image = try? host?.makeQRCode().generateQRCode()
     }
-
-
 }
 
